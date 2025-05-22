95922dc4c52e940db2d7c2bd2f1f92841588a1b0
Weslley Ceresa <webflix.tecnologia@gmail.com>
2025-05-22 17:11:58 -0300

    .

--- src/IO/SerializerInterface.php
+++ src/IO/SerializerInterface.php
@@ -5,6 +5,6 @@ namespace Ipag\Sdk\IO;
 interface SerializerInterface
 {
     function serialize(array $data): string;
-    function unserialize(string $data): array;
+    function unserialize(string $data): array|null;
     function getContentType(): ?string;
 }
